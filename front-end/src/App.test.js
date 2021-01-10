35590cae395d17aa0414dee93c8cbea2acce0ea5
Michele Beretta <beretta.m98@gmail.com>
2021-01-10 16:51:52 +0100

    added some tests

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
 });
